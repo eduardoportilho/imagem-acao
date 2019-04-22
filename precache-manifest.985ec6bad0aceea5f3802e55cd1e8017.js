15f10c2b914dbb2d78684d608b39378300dfb0e5
Eduardo Pilla Portilho <eduardo@portilho.com>
2019-04-21 22:35:20 -0300

    Updates

---
+++ precache-manifest.985ec6bad0aceea5f3802e55cd1e8017.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eaffbf52bc44a6fffe70",
+    "url": "/imagem-acao/static/js/runtime~main.59c4fa34.js"
+  },
+  {
+    "revision": "ea14ed93ce951c127fb5",
+    "url": "/imagem-acao/static/js/main.faae53f0.chunk.js"
+  },
+  {
+    "revision": "e1c8515704b91104fd6c",
+    "url": "/imagem-acao/static/js/2.34c6feb8.chunk.js"
+  },
+  {
+    "revision": "ea14ed93ce951c127fb5",
+    "url": "/imagem-acao/static/css/main.7ac7fde2.chunk.css"
+  },
+  {
+    "revision": "d334f859d2576307d6061ee61f48ab72",
+    "url": "/imagem-acao/index.html"
+  }
+];
